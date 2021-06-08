83a66781d0583334e2048ec7098c1409b2cd1de9
Francesco Paolo Piga <fppiga@outlook.com>
2021-06-08 14:38:01 +0200

    Add project files.

---
+++ BlazorMovies/Shared/Entities/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovies.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
