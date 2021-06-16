0dee5321119d4f5e972d7d912f87ba3c5d85943b
Francesco Paolo Piga <fppiga@outlook.com>
2021-06-16 10:14:43 +0200

    Added Counter component partial class

---
+++ BlazorMovies/Client/Pages/Counter.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject] 
+        SingletonService singleton { get; set; }
+        [Inject]
+        TransientService transient { get; set; }
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            singleton.Value += 1;
+            transient.Value += 1;
+        }
+    }
+}
