f1e55490dc28f84cb3a353a2e80c0bc3ef55b566
Francesco Paolo Piga <fppiga@outlook.com>
2021-06-24 12:07:21 +0200

    Added validation to Create Genre form

--- BlazorMovies/Shared/Entities/Genre.cs
+++ BlazorMovies/Shared/Entities/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BlazorMovies.Shared.Entities
     public class Genre
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="This field is required.")]
         public string Name { get; set; }
 
     }
