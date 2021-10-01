cee89d693eb03460227335101d08f9cd9595e192
Francesco Paolo Piga <fppiga@outlook.com>
2021-10-01 14:30:23 +0200

    Created Database with EF Core

--- BlazorMovies/Server/Startup.cs
+++ BlazorMovies/Server/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,7 +20,8 @@ namespace BlazorMovies.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
--- BlazorMovies/Shared/Entities/MoviesActors.cs
+++ BlazorMovies/Shared/Entities/MoviesActors.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BlazorMovies.Shared.Entities
 {
-    public class MoviesGenres
+    public class MoviesActors
     {
+        public int PersonId { get; set; }
         public int MovieId { get; set; }
-        public int GenresId { get; set; }
+        public Person Person { get; set; }
         public Movie Movie { get; set; }
-        public Genre Genre { get; set; }
-
+        public string Character { get; set; }
+        public int Order { get; set; }
     }
 }
