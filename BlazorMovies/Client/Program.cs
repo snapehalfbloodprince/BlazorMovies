b55e304efb3f8abee2d35d6bdd46fc7558e20314
Francesco Paolo Piga <fppiga@outlook.com>
2022-01-27 19:16:08 +0100

    * _Imports.razor: Started implementing authorization
    * App.razor:
    * Program.cs:
    * BlazorMovies.Client.csproj: Started implementing authorization

--- BlazorMovies/Client/Program.cs
+++ BlazorMovies/Client/Program.cs
@@ -1,5 +1,7 @@
+using BlazorMovies.Client.Auth;
 using BlazorMovies.Client.Helpers;
 using BlazorMovies.Client.Repository;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -15,7 +17,7 @@ namespace BlazorMovies.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             ConfigureServices(builder.Services);
             await builder.Build().RunAsync();
         }
@@ -27,6 +29,8 @@ namespace BlazorMovies.Client
             services.AddScoped<IHttpService, HttpService>();
             services.AddScoped<IPersonRepository, PersonRepository>();
             services.AddScoped<IMoviesRepository, MoviesRepository>();
+            services.AddAuthorizationCore();
+            services.AddScoped<AuthenticationStateProvider, DummyAuthenticationStateProvider>();
 
         }
     }
